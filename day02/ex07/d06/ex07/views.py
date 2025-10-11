0780d508c6d9265bac2b41d24108f85aa879581d
Essayegh, Othmane <offissayegh001@gmail.com>
2025-10-11 13:19:12 +0100

    chore: ex07

---
+++ day02/ex07/d06/ex07/views.py
@@ -0,0 +1,79 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from . import models
+
+
+def populate(request):
+    http_response_message = []
+    movies_data = (
+        (1, "The Phantom Menace", "George Lucas", "Rick McCallum", "1999-05-19"),
+        (2, "Attack of the Clones", "George Lucas", "Rick McCallum", "2002-05-16"),
+        (3, "Revenge of the Sith", "George Lucas", "Rick McCallum", "2005-05-19"),
+        (4, "A New Hope", "George Lucas", "Gary Kurtz, Rick McCallum", "1977-05-25"),
+        (
+            5,
+            "The Empire Strikes Back",
+            "Irvin Kershner",
+            "Gary Kurtz, Rick McCallum",
+            "1980-05-17",
+        ),
+        (
+            6,
+            "Return of the Jedi",
+            "Richard Marquand",
+            "Howard G. Kazanjian, George Lucas, Rick McCallum",
+            "1983-05-25",
+        ),
+        (
+            7,
+            "The Force Awakens",
+            "J. J. Abrams",
+            "Kathleen Kennedy, J. J. Abrams, Bryan Burk",
+            "2015-12-11",
+        ),
+    )
+    for movie in movies_data:
+        try:
+            if models.Movies.objects.filter(episode_nb=movie[0]).exists():
+                http_response_message.append(
+                    f"Movie with episode {movie[0]} already exists"
+                )
+            else:
+                row = models.Movies(
+                    episode_nb=movie[0],
+                    title=movie[1],
+                    director=movie[2],
+                    producer=movie[3],
+                    release_date=movie[4],
+                )
+                row.save()
+                http_response_message.append("OK")
+        except Exception as e:
+            http_response_message.append(f"Error: {e}")
+    return HttpResponse("<br>".join(http_response_message))
+
+
+def display(request):
+    try:
+        movies = models.Movies.objects.all()
+        if len(movies) == 0:
+            return HttpResponse("No data available")
+        return render(request, "table.html", {"movies": movies})
+    except Exception:
+        return HttpResponse("No data available")
+
+
+def update(request):
+    if request.method == "POST":
+        title = request.POST.get("movie_title")
+        opening_crawl = request.POST.get("opening_crawl")
+        models.Movies.objects.filter(title=title).update(opening_crawl=opening_crawl)
+        return redirect("update")
+
+    try:
+        titles_list = models.Movies.objects.values_list("title", flat=True)
+        if len(titles_list) == 0:
+            return HttpResponse("No data available")
+        return render(request, "dropdown.html", {"titles": titles_list})
+    except Exception:
+        return HttpResponse("No data available")
